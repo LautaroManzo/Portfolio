b7921841dc158cd799cb74592d3a093182a1b44d
Lautaro <lau_manzo12@hotmail.com>
2025-06-01 18:47:00 -0300

    Ajustes visuales

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const currentYear = new Date().getFullYear();
 
 const Footer = () => {
   return (
-    <footer className="px-6 pb-6">
+    <footer className="p-2 pt-4">
       <div className="w-full p-6 shadow-lg flex flex-row items-center justify-center rounded-lg bg-[#4793AF] text-[12px] sm:text-sm text-center font-semibold">
         <p className="text-gray-100">{currentYear} &nbsp;&nbsp; | &nbsp;&nbsp; lau_manzo12@hotmail.com</p>
       </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ import InProgress from "./CProjects/InProgress";
 
 const Projects = () => {
   return (
-    <div id="projects" className="w-full flex flex-col items-center gap-6 p-6 backdrop-blur-[3px]">
+    <div id="projects" className="w-full flex flex-col items-center gap-6 p-2 pt-6 backdrop-blur-[3px]">
 
       <div className="bg-[#4793AF] rounded-md w-full max-w-6xl px-6 shadow-lg">
         <h2 className="text-[22px] sm:text-4xl font-bold text-white py-4">
@@ -13,7 +13,7 @@ const Projects = () => {
         </h2>
       </div>
 
-      <div className="w-full flex flex-col items-center gap-8 px-2 sm:px-6">
+      <div className="w-full flex flex-col items-center gap-8 px-6 sm:px-6">
         <AdivinaLaPalabra />
         <InProgress />
       </div>
